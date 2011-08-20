d524c053cbd61e36b31ee3f9aa460be17419e232
Ash Charles <ash@gumstix.com>
2011-08-19 22:17:23 -0700

    Changes pulled from the older verdex port to match the verdex hardware more accurately

--- board/verdex/verdex.c
+++ board/verdex/verdex.c
@@ -58,7 +58,7 @@ extern void pxa_dram_init(void);
 int dram_init(void)
 {
 	pxa_dram_init();
-	gd->ram_size = PHYS_SDRAM_1_SIZE;
+	gd->ram_size = CONFIG_SYS_DRAM_SIZE;
 	return 0;
 }
 
@@ -66,11 +66,12 @@ void dram_init_banksize(void)
 {
 	gd->bd->bi_dram[0].start = PHYS_SDRAM_1;
 	gd->bd->bi_dram[0].size = PHYS_SDRAM_1_SIZE;
-
-#ifdef	CONFIG_RAM_256M
 	gd->bd->bi_dram[1].start = PHYS_SDRAM_2;
 	gd->bd->bi_dram[1].size = PHYS_SDRAM_2_SIZE;
-#endif
+	gd->bd->bi_dram[2].start = PHYS_SDRAM_3;
+	gd->bd->bi_dram[2].size = PHYS_SDRAM_3_SIZE;
+	gd->bd->bi_dram[3].start = PHYS_SDRAM_4;
+	gd->bd->bi_dram[3].size = PHYS_SDRAM_4_SIZE;
 }
 
 
